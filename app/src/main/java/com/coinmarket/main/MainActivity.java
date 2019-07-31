653f353b00df1be4bc08e841c5a2962ca6c7c497
belaya.ulyana <belaya.ulyana@gmail.com>
2019-07-31 03:51:13 +0300

    commit

--- app/src/main/java/com/coinmarket/main/MainActivity.java
+++ app/src/main/java/com/coinmarket/main/MainActivity.java
@@ -3,12 +3,18 @@ package com.coinmarket.main;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.coinmarket.R;
@@ -19,10 +25,12 @@ import com.coinmarket.listPOJO.Quote;
 
 import static android.support.constraint.Constraints.TAG;
 
-public class MainActivity extends AppCompatActivity implements MainContract {
+public class MainActivity extends AppCompatActivity implements MainContract, NavigationView.OnNavigationItemSelectedListener {
 
     private MainAdapter adapter;
     ProgressDialog progressDialog;
+    Toolbar toolbar;
+    DrawerLayout drawer;
 
     public void showInfo(NameValuePair nameValuePair) {
         adapter.addALL(nameValuePair.getData());
@@ -45,6 +53,12 @@ public class MainActivity extends AppCompatActivity implements MainContract {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.activity_main_title);
+
+        openDrawer();
+
         MainPresenter presenter = new MainPresenter(this);
         presenter.getDatum();
 
@@ -60,9 +74,6 @@ public class MainActivity extends AppCompatActivity implements MainContract {
             }
         });
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle(R.string.activity_main_title);
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
         recyclerView.setLayoutManager(layoutManager);
@@ -71,7 +82,26 @@ public class MainActivity extends AppCompatActivity implements MainContract {
         progressDialog = new ProgressDialog(MainActivity.this);
         progressDialog.setMessage("Loading....");
         progressDialog.show();
+    }
 
+
+    void openDrawer() {
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
@@ -79,4 +109,41 @@ public class MainActivity extends AppCompatActivity implements MainContract {
         getMenuInflater().inflate(R.menu.main_toolbar, menu);
         return true;
     }
+
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        if (item.getItemId() == android.R.id.action_settings) {
+//            drawer.openDrawer(GravityCompat.START);
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.favourites) {
+
+        } else if (id == R.id.list) {
+
+        } else if (id == R.id.notifications) {
+
+        } else if (id == R.id.news) {
+
+        } else if (id == R.id.remove_ads) {
+
+        } else if (id == R.id.settings) {
+
+        } else if (id == R.id.about_us) {
+
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 }
